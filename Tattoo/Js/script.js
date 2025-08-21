58fb8143f09f40faa9f52e95f01816dc06f849cd
Nisouzaa <souzanini@gmail.com>
2025-08-20 21:07:45 -0300

    quase la

--- Tattoo/Js/script.js
+++ Tattoo/Js/script.js
@@ -1,11 +1,30 @@
 // Menu responsivo
 function toggleMenu() {
-  document.getElementById("nav-links").classList.toggle("active");
+  document.getElementById("nav-links").classList.toggle("active"); 
 }
 
 // Formulário de cadastro
-document.getElementById("cadastroForm").addEventListener("submit", function(e){
-  e.preventDefault();
-  document.getElementById("msg-sucesso").style.display = "block";
-  this.reset();
+document
+  .getElementById("cadastroForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    document.getElementById("msg-sucesso").style.display = "block";
+    this.reset();
+  });
+
+// Logo
+const logoImg = document.querySelector("#loho img")
+
+logoImg.addEventListener("DOMContentLoaded", () => {
+  console.log = document.querySelector("#logo img");
+
+  setTimeout(() => {
+  document.querySelector("#logo img").classList.add("Show");
+  }, 300);
+
+  setTimeout(() => {
+    if (localStorage.getItem("mostrarElemento" === "true")) {
+      document.querySelector("#logo img").classList.add("Show");
+    }
+  }, 300);
 });

ab1e9d8b19ee8b136bbaba53e5905e7fa865fe66
Nisouzaa <souzanini@gmail.com>
2025-08-23 13:27:03 -0300

    Falta pouco

--- Tattoo/Js/script.js
+++ Tattoo/Js/script.js
@@ -1,30 +1,55 @@
 // Menu responsivo
 function toggleMenu() {
-  document.getElementById("nav-links").classList.toggle("active"); 
+  document.getElementById("nav-links").classList.toggle("active");
 }
 
 // Formulário de cadastro
 document
-  .getElementById("cadastroForm")
+  .getElementById('cadastroForm')
   .addEventListener("submit", function (e) {
     e.preventDefault();
-    document.getElementById("msg-sucesso").style.display = "block";
+    document.getElementById("msg-sucesso").style.display = 'block';
     this.reset();
-  });
+  });  
 
-// Logo
-const logoImg = document.querySelector("#loho img")
+// Logo Animad
+const logoImg = document.querySelector("#logo img");
 
 logoImg.addEventListener("DOMContentLoaded", () => {
-  console.log = document.querySelector("#logo img");
+  const logoImg = document.querySelector("#logo img");
 
   setTimeout(() => {
-  document.querySelector("#logo img").classList.add("Show");
+    logoImg.classList.add("Show");
   }, 300);
 
   setTimeout(() => {
     if (localStorage.getItem("mostrarElemento" === "true")) {
-      document.querySelector("#logo img").classList.add("Show");
+      logoImg.classList.add("Show");
     }
   }, 300);
+});
+
+// Script da galeria
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("img-ampliada");
+const fechar = document.querySelector(".fechar");
+const imagens = document.querySelectorAll(".galeria-item img");
+
+imagens.forEach(img => {
+  img.addEventListener("click", () => {
+    modal.classList.add("show");
+    modalImg.src = img.src;
+  });
+});
+
+fechar.addEventListener("click", () => {
+  modal.classList.remove("show");
+  setTimeout(() => (modal.style.display = "none"), 400);
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("show");
+    setTimeout(() => (modal.style.display = "none"), 400);
+  }
 });
